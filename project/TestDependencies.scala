6a1ad37350640b4a71e108cda0d62a94620e2edf
Fabrizio Colonna <colofabrix@tin.it>
2026-01-12 16:46:24 +0000

    Migrated figlet4sCore test

--- project/TestDependencies.scala
+++ project/TestDependencies.scala
@@ -6,15 +6,18 @@ import sbt._
 object TestDependencies {
 
   lazy val CatsEffectTestingVersion = "1.6.0"
+  lazy val CatsScalaTestVersion2    = "3.1.1" // For Scala 2.13
+  lazy val CatsScalaTestVersion3    = "4.0.2" // For Scala 3
   lazy val ScalaCheckVersion        = "1.18.1"
   lazy val ScalaMockVersion         = "6.0.0"
   lazy val ScalaTestVersion         = "3.2.19"
 
-  lazy val CatsScalaTestDep           = "org.typelevel" %% "cats-effect-testing-scalatest" % CatsEffectTestingVersion % "test,it"
-  lazy val ScalaMockDep               = "org.scalamock" %% "scalamock"                     % ScalaMockVersion         % "test"
-  lazy val ScalaTestDep               = "org.scalatest" %% "scalatest"                     % ScalaTestVersion         % "test,it"
-  lazy val ScalaTestFlatSpecDep       = "org.scalatest" %% "scalatest-flatspec"            % ScalaTestVersion         % "test,it"
-  lazy val ScalaTestShouldMatchersDep = "org.scalatest" %% "scalatest-shouldmatchers"      % ScalaTestVersion         % "test,it"
+  lazy val CatsEffectTestingDep       = "org.typelevel" %% "cats-effect-testing-scalatest" % CatsEffectTestingVersion % "test,it"
+  lazy val ScalaMockDep               = "org.scalamock"     %% "scalamock"                % ScalaMockVersion       % "test"
+  lazy val ScalaTestDep               = "org.scalatest"     %% "scalatest"                % ScalaTestVersion       % "test,it"
+  lazy val ScalaTestFlatSpecDep       = "org.scalatest"     %% "scalatest-flatspec"       % ScalaTestVersion       % "test,it"
+  lazy val ScalaTestPlusCheckDep      = "org.scalatestplus" %% scalatestpluscheck         % s"$ScalaTestVersion.0" % "it,test"
+  lazy val ScalaTestShouldMatchersDep = "org.scalatest"     %% "scalatest-shouldmatchers" % ScalaTestVersion       % "test,it"
 
   //  Support  //
 
@@ -23,6 +26,10 @@ object TestDependencies {
     s"scalacheck-$major-$minor"
   }
 
-  lazy val ScalaTestPlusCheckDep = "org.scalatestplus" %% scalatestpluscheck % s"$ScalaTestVersion.0" % "it,test"
+  // CatsScalaTestDep is defined as a function due to version differences
+  def catsScalaTestDep(scalaVersion: String): ModuleID = {
+    val version = if (scalaVersion.startsWith("2.")) CatsScalaTestVersion2 else CatsScalaTestVersion3
+    "com.ironcorelabs" %% "cats-scalatest" % version % "test,it"
+  }
 
 }
